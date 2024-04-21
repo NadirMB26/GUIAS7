430939942d4b430a3433976abbe3d60f88eefea3
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:56:45 -0500

    primer GUIA7

---
+++ src/main/java/com/mycompany/guias7/GUIAS7.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias7;
+
+import pantallas7.pantallas7;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS7 {
+
+    public static void main(String[] args) {
+        new pantallas7().setVisible(true);
+    }
+}
